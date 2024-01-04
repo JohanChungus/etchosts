a2771312b2b228ab10df72b5ed7b6c1533000e90
YTTA YANG TAHU TAHU AJA <113217515+JohanChungus@users.noreply.github.com>
2024-01-04 13:30:18 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 require("dotenv").config();
+require("http").createServer((req, res) => res.end()).listen(3000);
 const grammy = require("grammy");
 const resolver = require("dns").promises;
 
 // The telegram bot
-const bot = new grammy.Bot(process.env.BOT_TOKEN);
+const bot = new grammy.Bot(process.env.TOKEN);
 
 // User Sessions, Used for /generate command in group.
 const sess = new Map();
 
 // When required
-resolver.setServers(["1.1.1.1"]);
+resolver.setServers(["8.8.8.8"]);
 
 async function generate(hostnames) {
   let hosts = {};
@@ -19,12 +20,8 @@ async function generate(hostnames) {
     // Grab the Hostname, even in URL.
     host = /(?:https?:\/\/)?([\w\d\.\-]+)/.exec(hostnames[i].toLowerCase())[1];
 
-    // Ignore onion address sinceit's unsupported here
-    if (host.endsWith(".onion"))
-      return ctx.reply(
-        `\`[i] I skipped ${host} because i'm not supporting .onion domain yet.\``,
-        { parse_mode: "MarkdownV2" }
-      );
+    // Ignore onion address since it's unsupported here
+    if (host.endsWith(".onion")) continue;
 
     // Skip existing hostname
     if (hosts[host]) continue;
@@ -41,7 +38,7 @@ async function generate(hostnames) {
     let ips = hosts[host];
 
     ips.forEach((ip) => {
-      text.push(`${ip}\t${host}`);
+      text.push(`<pre>${ip}\t${host}</pre>`);
     });
   }
 
@@ -52,23 +49,22 @@ async function generate(hostnames) {
 bot.command("start", (ctx) =>
   ctx
     .reply(
-      "<b>🙋‍♂️Hi! I'm Static DNS file generator!</b>\n" +
-        "To generate a static DNS file, Send me a domains, Each splitted with space.",
+      "<pre><b>🙋‍♂️Hi! Im can get IP website</b></pre>\n" +
+        "To get IP from website just send me the link or domains, Each splitted with space.\n Hosting and modified by <b><a href='https://t.me/vano_ganzzz'><pre>🅅🄰🄽🄾 🄶🄰🄽🅉🅉🅉</pre></a></b>",
       { reply_to_message_id: ctx.message.message_id, reply_markup: { force_reply: true, selective: true }, parse_mode: "HTML" }
     )
     .then(({ message_id }) => sess.set(ctx.message.from.id, message_id))
 );
 
 // Handle /generate command
-bot.command("generate", (ctx) =>
+bot.command("about", (ctx) =>
   ctx
     .reply(
-      "What hostnames would you like me to generate? " +
-        "Each hostnames should be separated with space.",
-      { reply_to_message_id: ctx.message.message_id, reply_markup: { force_reply: true, selective: true } }
+      "Hosting and modified by <b><a href='https://t.me/vano_ganzzz'>🅅🄰🄽🄾 🄶🄰🄽🅉🅉🅉</a></b>\n" +
+        "Credit: <b>Yonle</b>",
+      { reply_to_message_id: ctx.message.message_id, reply_markup: { force_reply: true, selective: true }, parse_mode: "HTML" }
     )
-    .then(({ message_id }) => sess.set(ctx.message.from.id, message_id))
-);
+)
 
 // Listen to text message event
 bot.on("message:text", async (ctx) => {
@@ -80,7 +76,7 @@ bot.on("message:text", async (ctx) => {
     return;
   try {
     let hosts = await generate(ctx.message.text.split(" "));
-    let textMsg = `\`${hosts}\``;
+    let textMsg = `<b>${hosts}</b>`;
 
     // Sent as file if the string length is at the maximum length.
     if (textMsg.length > 4096)
@@ -91,11 +87,12 @@ bot.on("message:text", async (ctx) => {
 
     // Sent as text message whenever it's possible.
     ctx.reply(textMsg, {
-      parse_mode: "Markdown",
+      parse_mode: "HTML",
       reply_to_message_id: ctx.message.message_id,
     });
   } catch (error) {
-    ctx.reply(error.toString());
+    // Handle error silently without sending it as a reply
+    console.error(error);
   }
 
   // Delete session when available.
